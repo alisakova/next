1542a648ea9ced56eeefc10c05c362b24d6cefa1
alisakova <coctail_rossini@mail.ru>
2022-07-07 00:51:34 +0300

    start slack bot

--- netlify/functions/slack.js
+++ netlify/functions/slack.js
@@ -1,6 +1,34 @@
+import * as dotenv from 'dotenv';
+import { App, ExpressReceiver } from '@slack/bolt';
+
+dotenv.config();
+
+const expressReceiver = new ExpressReceiver({
+  signingSecret: `${process.env.SLACK_SIGNING_SECRET}`,
+  processBeforeResponse: true
+});
+
+const app = new App({
+  signingSecret: `${process.env.SLACK_SIGNING_SECRET}`,
+  token: `${process.env.SLACK_BOT_TOKEN}`,
+  receiver: expressReceiver
+});
+
+const parseRequestBody = (stringBody) => {
+  try {
+    return JSON.parse(stringBody ?? "");
+  } catch {
+    return undefined;
+  }
+}
+
 exports.handler = async function(event, context) {
-  return {
-    statusCode: 200,
-    body: "Hello, World"
-  };
+  const payload = parseRequestBody(event.body);
+
+  if (payload && payload.type && payload.type === 'url_verification') {
+    return {
+      statusCode: 200,
+      body: payload.challenge
+    };
+  }
 }
