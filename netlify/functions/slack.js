67de9d3a4c22e3fbf886695e83c00a1fae281aaa
alisakova <coctail_rossini@mail.ru>
2022-07-07 01:00:02 +0300

    add slash command

--- netlify/functions/slack.js
+++ netlify/functions/slack.js
@@ -26,6 +26,16 @@ app.message(async ({ say }) => {
   await say("Hi :wave:");
 });
 
+app.command('/greet', async({body, ack}) => {
+  ack();
+  await app.client.chat.postEphemeral({
+    token: process.env.SLACK_BOT_TOKEN,
+    channel: body.channel_id,
+    text: "Greetings, user!" ,
+    user: body.user_id
+  });
+});
+
 exports.handler = async function(event, context) {
   const payload = parseRequestBody(event.body);
 
