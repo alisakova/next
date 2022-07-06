327c1f193272bde60603e26eaefad8634704c968
alisakova <coctail_rossini@mail.ru>
2022-07-07 00:25:09 +0300

    netlify function

--- next.config.js
+++ next.config.js
@@ -6,13 +6,4 @@ module.exports = {
     config.resolve.modules.push(path.resolve("./"));
     return config;
   },
-  i18n: {
-    // These are all the locales you want to support in
-    // your application
-    locales: ["en", "ru"],
-    // This is the default locale you want to be used when visiting
-    // a non-locale prefixed path e.g. `/hello`
-    defaultLocale: "en",
-    localeDetection: false,
-  },
 };
