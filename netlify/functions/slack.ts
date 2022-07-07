538f3ec774cfde83f8128917a8a677f516d4c760
alisakova <coctail_rossini@mail.ru>
2022-07-08 00:13:54 +0300

    change action name

--- netlify/functions/slack.ts
+++ netlify/functions/slack.ts
@@ -141,9 +141,10 @@ app.command('/start', async ({ say, body, ack }) => {
   // });
 });
 
-app.action('button_click', async ({ payload, say, ack, body, logger }) => {
+app.action('block_actions', async ({ payload, say, ack, body, logger }) => {
   ack();
   logger.info(payload, body);
+  console.log(payload, body);
   await say("Deploy preview for any merge request is on :fire:");
 });
 
