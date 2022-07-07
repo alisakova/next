9bea709b7c19a523abc3019058cc48a59d3ce0eb
alisakova <coctail_rossini@mail.ru>
2022-07-07 19:22:31 +0300

    fix error

--- netlify/functions/slack.ts
+++ netlify/functions/slack.ts
@@ -1,6 +1,6 @@
 import * as dotenv from 'dotenv';
 import fetch from "node-fetch";
-import { App, ExpressReceiver } from '@slack/bolt';
+import { App, ExpressReceiver, ReceiverEvent } from '@slack/bolt';
 
 dotenv.config();
 
@@ -15,36 +15,40 @@ const app = new App({
   receiver: expressReceiver
 });
 
-const parseRequestBody = (stringBody, contentType) => {
+const parseRequestBody = (stringBody: string | null, contentType: string | undefined): any | undefined => {
   try {
-    let inputStringBody = stringBody ?? "";
-    let result = {};
-
-    if (contentType && contentType === 'application/x-www-form-urlencoded') {
-      const keyValuePairs = inputStringBody.split('&');
-      keyValuePairs.forEach((pair) => {
-        let individualKeyValuePair = pair.split('=');
-        result[individualKeyValuePair[0]] = decodeURIComponent(individualKeyValuePair[1] || '');
-      });
-      return JSON.parse(JSON.stringify(result));
-    } else {
-      return JSON.parse(inputStringBody);
+    if (!stringBody) {
+      return "";
     }
+
+    let result: any = {};
+
+    if (contentType && contentType === "application/json") {
+      return JSON.parse(stringBody);
+    }
+
+    let keyValuePairs: string[] = stringBody.split("&");
+    keyValuePairs.forEach(function (pair: string): void {
+      let individualKeyValuePair: string[] = pair.split("=");
+      result[individualKeyValuePair[0]] = decodeURIComponent(individualKeyValuePair[1] || "");
+    });
+    return JSON.parse(JSON.stringify(result));
+
   } catch {
-    return undefined;
+    return "";
   }
 }
 
-const replyReaction = async(channelId, messageThreadTs) => {
+const replyReaction = async (channelId, messageThreadTs) => {
   try {
-      await app.client.reactions.add({
-          token: process.env.SLACK_BOT_TOKEN,
-          name: 'robot_face',
-          channel: channelId,
-          timestamp: messageThreadTs,
-      });
+    await app.client.reactions.add({
+      token: process.env.SLACK_BOT_TOKEN,
+      name: 'heart',
+      channel: channelId,
+      timestamp: messageThreadTs,
+    });
   } catch (error) {
-      console.error(error);
+    console.error(error);
   }
 }
 
@@ -60,34 +64,34 @@ const TURN_ON_BUILD_PAYLOAD = {
   cdp_enabled: false
 };
 
-app.command('/turn-on-build', async({body, ack}) => {
+app.command('/turn-on-build', async ({ body, ack }) => {
   ack();
-  try  {
+
+  try {
     await fetch("https://app.netlify.com/access-control/bb-api/api/v1/sites/8e9faadc-ba17-49b8-b9e5-b333bd2ba4eb", {
       method: "PUT",
       headers: {
         Authorization: process.env.BEARER_TOKEN,
       },
       body: JSON.stringify(TURN_ON_BUILD_PAYLOAD),
-    }).then(async () => {
-      await app.client.chat.postEphemeral({
-        token: process.env.SLACK_BOT_TOKEN,
-        channel: body.channel_id,
-        text: "Билд превью включился" ,
-        user: body.user_id
-      });
+    });
+    await app.client.chat.postEphemeral({
+      token: process.env.SLACK_BOT_TOKEN,
+      channel: body.channel_id,
+      text: "Билд превью включился",
+      user: body.user_id
     });
   } catch (error) {
     await app.client.chat.postEphemeral({
       token: process.env.SLACK_BOT_TOKEN,
       channel: body.channel_id,
-      text: "Запрос не прошел, повторите позже" ,
+      text: "Запрос не прошел, повторите позже",
       user: body.user_id
     });
   }
 });
 
-export async function handler(event, context) {
+export async function handler(event) {
   const payload = parseRequestBody(event.body, event.headers["content-type"]);
 
   if (payload && payload.type && payload.type === 'url_verification') {
@@ -97,7 +101,7 @@ export async function handler(event, context) {
     };
   }
 
-  const slackEvent = {
+  const slackEvent: ReceiverEvent = {
     body: payload,
     ack: async (response) => {
       return new Promise((resolve, reject) => {
@@ -111,4 +115,9 @@ export async function handler(event, context) {
   };
 
   await app.processEvent(slackEvent);
+
+  return {
+    statusCode: 200,
+    body: ""
+  };
 }
